fffcd67aea173e7109d54362fb6d9c6cbbdcb108
joonmo <jmpark6846@gmail.com>
2021-09-14 18:35:09 +0900

    allowed host 변경

--- smallissue/settings/production.py
+++ smallissue/settings/production.py
@@ -1,4 +1,4 @@
 from .base import *
 
-ALLOWED_HOSTS = ['smallissue.eba-p5pcrsre.ap-northeast-2.elasticbeanstalk', ]
+ALLOWED_HOSTS = ['smallissue.eba-p5pcrsre.ap-northeast-2.elasticbeanstalk.com', ]
 DEBUG = False
