bb26ec7e32f7ee9a162d8f6f6e70383af5903415
joonmo <jmpark6846@gmail.com>
2021-09-14 18:04:28 +0900

    production allowed hosts 수정

--- smallissue/settings/production.py
+++ smallissue/settings/production.py
@@ -1,4 +1,4 @@
 from .base import *
 
-ALLOWED_HOSTS = ['.smallissue-backend.eba-ab4pe3zw.us-west-2.elasticbeanstalk.com', ]
+ALLOWED_HOSTS = ['smallissue-backend.eba-ab4pe3zw.us-west-2.elasticbeanstalk.com', ]
 DEBUG = False
