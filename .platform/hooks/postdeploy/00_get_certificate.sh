174b75a812873ac35fbebb00804f6fce3eb3e5c0
joonmo <jmpark6846@gmail.com>
2021-09-18 19:43:19 +0900

    change url

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo certbot -n -d smallissue.eba-4uzry5z8.ap-northeast-2.elasticbeanstalk.com --nginx --agree-tos --email jmpark6846@naver.com
+sudo certbot -n -d www.smallissue.eba-4uzry5z8.ap-northeast-2.elasticbeanstalk.com --nginx --agree-tos --email jmpark6846@naver.com
