413de8cd03fdb7b139076da3affcca61124843f6
joonmo <jmpark6846@gmail.com>
2021-09-20 10:09:26 +0900

    자잘한 변경
    자잘한 변경
    
    자잘한 변경
    
    자잘한 변경
    
    자잘한 변경
    
    자잘한 변경
    
    자잘한 변경
    
    자잘한 변경
    
    자잘한 변경
    
    자잘한 변경
    
    자잘한 변경
    
    자잘한 변경
    
    자잘한 변경
    
    자잘한 변경

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-sudo certbot -n -d smallissue.app --nginx --agree-tos --email jmpark6846@naver.com
+sudo docker stop `docker ps -q`
+sudo certbot -n -d smallissue.app -d www.smallissue.app --expand --nginx --agree-tos --email jmpark6846@naver.com
+sudo fuser -k 80/tcp
+sudo docker start current_web_1
+sudo docker start current_nginx_1
--- smallissue/settings/production.py
+++ smallissue/settings/production.py
@@ -1,5 +1,5 @@
 from .base import *
 
-ALLOWED_HOSTS = ['smallissue.eba-4uzry5z8.ap-northeast-2.elasticbeanstalk.com', ]
+ALLOWED_HOSTS = ['smallissue.eba-4uzry5z8.ap-northeast-2.elasticbeanstalk.com', 'smallissue.app']
 JWT_AUTH_SECURE = True
 DEBUG = False
