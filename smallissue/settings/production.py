9f7b74c057ff5b384265235e9fec24a815a57abc
joonmo <jmpark6846@gmail.com>
2021-09-21 19:14:12 +0900

    서버 스크립트 수정

--- smallissue/settings/production.py
+++ smallissue/settings/production.py
@@ -1,5 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['smallissue.eba-4uzry5z8.ap-northeast-2.elasticbeanstalk.com', 'smallissue.app']
+ALLOWED_HOSTS = ['smallissue.eba-4uzry5z8.ap-northeast-2.elasticbeanstalk.com', 'smallissue.app',
+                 'awseb-e-f-AWSEBLoa-1EUNDVO81EY6P-1847361572.ap-northeast-2.elb.amazonaws.com']
 JWT_AUTH_SECURE = True
 DEBUG = False
