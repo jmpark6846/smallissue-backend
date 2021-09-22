2147c6f814abd220fd08f9f571cc4693056a5102
joonmo <jmpark6846@gmail.com>
2021-09-22 19:46:08 +0900

    api.smallissue.app 으로 주소 변경

--- smallissue/settings/production.py
+++ smallissue/settings/production.py
@@ -2,8 +2,7 @@ from .base import *
 
 ALLOWED_HOSTS = ['smallissue.eba-4uzry5z8.ap-northeast-2.elasticbeanstalk.com',
                 'awseb-e-f-AWSEBLoa-1EUNDVO81EY6P-1847361572.ap-northeast-2.elb.amazonaws.com',
-                'smallissue.app',
-                'www.smallissue.app']
+                'api.smallissue.app',]
 
 JWT_AUTH_SECURE = True
 DEBUG = False
