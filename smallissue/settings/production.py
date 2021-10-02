90097bc8661aa155ffccd97ade1b233af5c86fb6
joonmo <jmpark6846@gmail.com>
2021-10-02 18:39:23 +0900

    allowed host 변경

--- smallissue/settings/production.py
+++ smallissue/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 print('production settings loaded')
 ALLOWED_HOSTS = ['smallissue.eba-4uzry5z8.ap-northeast-2.elasticbeanstalk.com',
                 'awseb-e-f-AWSEBLoa-1EUNDVO81EY6P-1847361572.ap-northeast-2.elb.amazonaws.com',
-                'api.smallissue.app',]
+                'api.smallissue.app', 'smallissue.app']
 
 JWT_AUTH_SECURE = True
 DEBUG = False
